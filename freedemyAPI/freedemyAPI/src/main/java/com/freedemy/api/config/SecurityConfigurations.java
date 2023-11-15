3003f7faee65381d0bffefa96322be461469dbe2
Kirtiman <bhagawati.kirtiman@gmail.com>
2023-11-15 11:07:14 +0530

    CSRF token added, although needs few changes in the protected urls list in the csrf method when clubbed with the frontend application

--- freedemyAPI/freedemyAPI/src/main/java/com/freedemy/api/config/SecurityConfigurations.java
+++ freedemyAPI/freedemyAPI/src/main/java/com/freedemy/api/config/SecurityConfigurations.java
@@ -1,19 +1,51 @@
 package com.freedemy.api.config;
 
+import com.freedemy.api.filters.CsrfCookieFilter;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.security.web.csrf.CsrfTokenRequestAttributeHandler;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+
+import java.util.Collections;
 
 @Configuration
 public class SecurityConfigurations {
 
     @Bean
     public SecurityFilterChain defaultSecurityFilterChain(HttpSecurity httpSecurity) throws Exception{
-        httpSecurity.csrf().disable()
+
+        CsrfTokenRequestAttributeHandler csrfTokenRequestAttributeHandler = new CsrfTokenRequestAttributeHandler();
+        csrfTokenRequestAttributeHandler.setCsrfRequestAttributeName("_csrf");
+        httpSecurity.securityContext().requireExplicitSave(false) //need to dig more to understand
+                .and().sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.ALWAYS))//need to dig more to understand
+                .cors().configurationSource(new CorsConfigurationSource() {
+                    @Override
+                    public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+                        CorsConfiguration config = new CorsConfiguration();
+                        config.setAllowedOrigins(Collections.singletonList("http://localhost:4200")); //list of servers from where the API will be called
+                        config.setAllowedMethods(Collections.singletonList("*"));
+                        config.setAllowCredentials(true);
+                        config.setAllowedHeaders(Collections.singletonList("*"));
+                        config.setMaxAge(3600L);
+
+                        return config;
+                    }
+                })
+                //.and().csrf().ignoringRequestMatchers("/signup") //get methods are not protected against csrf, hence can be omitted from the list, we can ignore csrf protection against public APIs like signup etc
+                //.and().csrf().disable() //disabling csrf is never recommended
+                .and().csrf((csrf)->csrf.csrfTokenRequestHandler(csrfTokenRequestAttributeHandler).ignoringRequestMatchers("/signup","/addcourse")
+                        .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())) //withHttpOnlyFalse() is used to make sure Angular JS frontend can read the values
+                .addFilterAfter(new CsrfCookieFilter(), BasicAuthenticationFilter.class)//execute csrfcookiefilter after basicautehnticationfilter
                 .authorizeHttpRequests((requests) -> requests
                         .requestMatchers("/addcourse","/myBalance","/myLoans","/myCards").authenticated()
                         .requestMatchers("/courses","/contact","/signup").permitAll())
--- freedemyAPI/freedemyAPI/src/main/java/com/freedemy/api/rest/controllers/LoginController.java
+++ freedemyAPI/freedemyAPI/src/main/java/com/freedemy/api/rest/controllers/LoginController.java
@@ -4,11 +4,19 @@ import com.freedemy.api.models.Customer;
 import com.freedemy.api.repositories.CustomerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
+//CrossOrigin annotation to override the CORS layer of the browser
+//instead of asterisk, can have a full url like https://localhost:9999/something
+//asterisk means all the urls are allowed to communicate with the API
+//this is not a recommended way, as it'll be tedious to add this annotation to all the controllers
+//recommended way is to create a global configuration in the SecurityFilterChain bean
+//@CrossOrigin(origins = "*")
 @RestController
 public class LoginController {
 
@@ -37,5 +45,15 @@ public class LoginController {
         }
 
         return response;
+    }
+
+    @GetMapping("/user")
+    public Customer getUserDetailsAfterLogin(Authentication authentication) {
+        List<Customer> customers = customerRepository.findByUsername(authentication.getName());
+        if (customers.size() > 0) {
+            return customers.get(0);
+        } else {
+            return null;
+        }
     }
 }
